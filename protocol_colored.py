254c5fc9009c8ca57b9150ec58d96877dbb675cc
sgounde1 <sgounde1@jhu.edu>
2026-07-22 17:15:14 -0400

    Expanding on dilution tutorial by adding color to liquid. Unfortunately, the Opentrons app doesn't actually visually show the color gradient but it does maintain the fraction of dilutent to sample.

--- protocol_colored.py
+++ protocol_colored.py
@@ -1,11 +1,9 @@
 from opentrons import protocol_api
 
 metadata = {
-	"protocolName": "Serial Dilution Tutorial",
-	"description": """This protocol is the outcome of following the
-        	Python Protocol API Tutorial located at
-        	https://docs.opentrons.com/v2/tutorial.html. It takes a solution
-        	and progressively dilutes it by transferring it stepwise across a plate.""",
+	"protocolName": "Serial Dilution with Color",
+	"description": """This protocol is an extension of the dilution tutorial executed
+		by protocol.py. It shows the dilution of a colored sample in a clear dilutent.""",
 	"author": "Smriti Gounder"
 }
 
@@ -20,11 +18,26 @@ def run(protocol: protocol_api.ProtocolContext):
   plate = protocol.load_labware("nest_96_wellplate_200ul_flat", "D3")
   trash = protocol.load_trash_bin("A3")
   left_pipette = protocol.load_instrument("flex_1channel_1000", "left", tip_racks=[tips])
-  
+
+  # Define liquids with colors
+  dilutent = protocol.define_liquid(
+    name="Diluent",
+    description="Diluent buffer",
+    display_color = None
+  )
+  sample = protocol.define_liquid(
+    name="Sample",
+    description="Concentrated sample solution",
+    display_color= "#FF0000"
+  )
+
+  # Load liquids into wells (volume in µL)
+  reservoir["A1"].load_liquid(liquid=dilutent, volume=15000)
+  reservoir["A2"].load_liquid(liquid=sample, volume=15000)
+
   left_pipette.transfer(100, reservoir["A1"], plate.wells())
 
   for i in range(8):
     row = plate.rows()[i]
     left_pipette.transfer(100, reservoir["A2"], row[0], mix_after=(3, 50))
     left_pipette.transfer(100, row[:11], row[1:], mix_after=(3, 50))
-
